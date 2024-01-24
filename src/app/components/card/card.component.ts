3f1f815398bf744abc7e5f32a6aae15f47cf2cab
matheus-ts <matheus.csilva@bandtec.com.br>
2024-01-24 17:34:34 -0300

    Feature: card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Currency } from 'src/app/helpers/ENUM';
+import { CurrencyData } from 'src/app/interfaces/currency.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input() currency?: CurrencyData;
+  @Input() isLoading: boolean = false;
+  @Input() hasError: boolean = false;
+  @Input() code?: string;
 
-  constructor() { }
+  @Output() refresh: EventEmitter<string> = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getClassForValue(value: any): string {
+    const roundedValue: number = parseFloat(value);
+    if (roundedValue <= 1.0) {
+      return 'text-danger';
+    }
+    if (roundedValue > 1.0 && roundedValue <= 5.0) {
+      return 'text-info';
+    }
+    if (roundedValue > 5.0) {
+      return 'text-success';
+    }
+    return '';
   }
 
+  emitEvent(): void {
+    return this.refresh.emit(this.code);
+  }
 }
